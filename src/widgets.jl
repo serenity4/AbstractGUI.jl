27d4e857b45cf129526687bd24a42249a6eabc58
serenity4 <cedric.bel@hotmail.fr>
2021-03-14 17:24:33 +0100

    Redefine widget type

--- src/widgets.jl
+++ src/widgets.jl
@@ -1,6 +1,16 @@
-"Abstract supertype for widgets"
-abstract type Widget end
+"""
+Renderable object which, when consumed, returns a value of type `T`.
 
-abstract type Layout end
+A widget is a consumable and immutable data structure rendered to the screen.
+Upon trigger of certain registered events, the widget finishes and yields a unique value of type `T`.
 
-function render(image::Image, w::Widget) end
+"""
+abstract type Widget{T} end
+
+eventtype(::Type{Widget{T}}) where {T} = T
+eventtype(a::Widget) = eventtype(typeof(a))
+
+function compose(a::W{T1}, b::W{T2}) where {W <: Widget,T1,T2}
+    res = compose_event(a, b)
+    W{Union{T1,T2}}(res)
+end
